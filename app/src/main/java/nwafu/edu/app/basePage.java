52f6f92bbd603a02ff0ff67cec40206cc204b3cc
CH3OCH3-lio <70514558+CH3OCH3-lio@users.noreply.github.com>
2022-06-05 22:39:38 +0800

    Initial commit

---
+++ app/src/main/java/nwafu/edu/app/basePage.java
@@ -0,0 +1,14 @@
+package nwafu.edu.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class basePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_page);
+    }
+}
